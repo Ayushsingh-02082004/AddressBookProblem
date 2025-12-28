57915380251374ffeb6c2caea3711c7736551f15
Ayush singh <ayushsingh02082004@gmail.com>
2025-12-28 14:45:16 +0530

    uc 10 commit

--- AddressManagemement/services/AddressBookSystem.cs
+++ AddressManagemement/services/AddressBookSystem.cs
@@ -31,7 +31,9 @@ namespace AddressManagemement.services
                 Console.WriteLine("press 5 to search person from state");
                 Console.WriteLine("press 6 to view all person in a city");
                 Console.WriteLine("press 7 to view all person in a state");
-                Console.WriteLine("press 8 to Exit from the library");
+                Console.WriteLine("press 8 to get the count of contact in city");
+                Console.WriteLine("press 9 to get the count of contact in state");
+                Console.WriteLine("press 10 to Exit from the library");
 
                 Console.WriteLine("Choose the option : ");
                 string choice = Console.ReadLine();
@@ -60,6 +62,12 @@ namespace AddressManagemement.services
                         ViewPersonByState();
                         break;
                     case "8":
+                        CountByCity();
+                        break;
+                    case "9":
+                        CountByState();
+                        break;
+                    case "10":
                         flag = false;
                         Console.WriteLine("Exited successfully from the library.");
                         break;
@@ -210,6 +218,37 @@ namespace AddressManagemement.services
                     }
                 }
             }
+        }
+
+        public void CountByCity()
+        {
+            BuildCityStateDictionary();
+            Console.WriteLine("Enter cityname to get count ");
+            String city = Console.ReadLine();
+
+            if (CityiDictionary.ContainsKey(city))
+            {
+                int Count = CityiDictionary[city].Count;
+                Console.WriteLine($"Count of contacts in the city : {city} is : {Count}");
+            }
+
+            else Console.WriteLine($"There is no city named {city} .");
+        }
+
+        public void CountByState()
+        {
+            BuildCityStateDictionary();
+            Console.WriteLine("Enter the state name to get count");
+
+            String state = Console.ReadLine();
+
+            if (StateDictionary.ContainsKey(state))
+            {
+                int count = StateDictionary[state].Count;
+                Console.WriteLine($"Count of the contact in state : {state} is : {count}");
+            }
+
+            else Console.WriteLine($"There is no state named {state}");
         }
 
     }
